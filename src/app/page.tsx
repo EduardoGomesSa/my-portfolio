a87aeb8cdf843259abf6a4506cf08001a241ecd9
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-14 21:15:25 -0300

    remoção de código teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Teste from "../components/teste";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Aqui vai ficar as paradas</h1>
-      <Teste />
+      
     </main>
   );
 }
