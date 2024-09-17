56438f0c06b1aba6a01f35b3f9ab20bbece51a75
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-16 23:23:09 -0300

    footer no fundo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,9 @@
 export function Footer() {
     var currentYar = new Date().getFullYear()
     return (
-        <p>&copy;Eduardo Sa - { currentYar }</p>
+        <div className="flex justify-center items-center bg-red-500 w-full h-[100px] fixed bottom-0 left-0">
+            <p>&copy;Eduardo Sa - { currentYar }</p>
+        </div>
+        
     );
 }
