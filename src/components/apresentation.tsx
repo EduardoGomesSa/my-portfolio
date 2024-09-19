a1d2ee729af56956b1701c200f975e9c3f489a09
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-18 23:55:02 -0300

    Mudança na cor dos botões

--- src/components/apresentation.tsx
+++ src/components/apresentation.tsx
@@ -9,10 +9,10 @@ export function Apresentation() {
             </p>
 
             <div className="flex gap-4">
-            <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
+            <button className="bg-[#413A4F] text-white py-2 px-4 rounded hover:bg-blue-700">
                 Meu currículo
             </button>
-            <button className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-700">
+            <button className="bg-[#8A42DB] text-white py-2 px-4 rounded hover:bg-green-700">
                 Entre em contato
             </button>
         </div>
