28a03147c31bedb008356578b1ee6a0d247750ff
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-18 23:35:56 -0300

    definindo o tamanho do titulo

--- src/components/apresentation.tsx
+++ src/components/apresentation.tsx
@@ -1,7 +1,7 @@
 export function Apresentation() {
     return (
         <div>
-           <h1>Olá, Eu Sou Eduardo</h1>
+           <h1 className="text-4xl">Olá, Eu Sou Eduardo</h1>
             <h2>Desenvolvedor Full-stack e entusasta de tecnologia e inovação</h2>
             <p>Já estou na área a mais de 2 anos. Trabalho com desenvilmento de aplicativos e sites, utilizando as seguintes tecnologias Flutter, React, Laravel e MySQL</p>
         </div>
