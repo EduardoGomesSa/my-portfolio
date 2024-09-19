aa2ee02719a448b3b42e0ab74eada75a0b0b208d
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-19 00:02:08 -0300

    Configurando o grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Header } from "@/components/header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#171023]">
+    <main className="grid grid-rows-[auto,1fr,auto] min-h-screen bg-[#171023] p-24 gap-8">
       <Header />
       <Apresentation />
       <h1>Aqui vai ficar as paradas</h1>
