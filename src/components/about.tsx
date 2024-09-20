7ca3f0060dd57d8bbd8ef62615605f49cc6c5313
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-20 01:27:05 -0300

    Background branco com opacity

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,19 +1,22 @@
 export function About() {
     return (
-        <div>
-            <div>
-                <p>tecnologias</p>
+        <div className="w-full h-full flex justify-center items-center">
+            <div className="w-[90vw] bg-white bg-opacity-10 p-8 rounded-lg shadow-lg mx-auto mt-10">
+                <div>
+                    <p>tecnologias</p>
+                </div>
+                <p>Sobre mim</p>
+                <p>Eu sou um aficcionado por desenvolvimento de software e tecnologia. Sempre em busca de novos desefios </p>
+                <p>Beyond coding, I'm a coffee enthusiast, a cat lover, and a self-taught artist who enjoys
+                    spending my free time doodling. I am currently seeking opportunities to bring my skills
+                    and enthusiasm to a tech company in the United States or Europe and am
+                    excited about the prospect of relocating to pursue new challenges.
+                </p>
+                <button className="bg-[#8A42DB] text-white py-2 px-4 rounded hover:bg-blue-700">
+                    Meu currículo
+                </button>
             </div>
-            <p>Sobre mim</p>
-            <p>Eu sou um aficcionado por desenvolvimento de software e tecnologia. Sempre em busca de novos desefios </p>
-            <p>Beyond coding, I'm a coffee enthusiast, a cat lover, and a self-taught artist who enjoys
-                spending my free time doodling. I am currently seeking opportunities to bring my skills
-                and enthusiasm to a tech company in the United States or Europe and am
-                excited about the prospect of relocating to pursue new challenges.
-            </p>
-            <button className="bg-[#8A42DB] text-white py-2 px-4 rounded hover:bg-blue-700">
-                Meu currículo
-            </button>
         </div>
+
     )
 }
