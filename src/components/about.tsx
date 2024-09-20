85f3fcd7af2e5674f2454c02088be92daa8d7fe8
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-20 01:16:31 -0300

    Estrutura do About

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,28 +1,19 @@
 export function About() {
     return (
         <div>
-            Aqui ficarão as informações sobre
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam assumenda voluptate illum consequuntur exercitationem deserunt natus laboriosam corrupti consectetur nam aperiam repellat similique ea, velit excepturi? Atque, molestiae eius?</p>
-
-            
+            <div>
+                <p>tecnologias</p>
+            </div>
+            <p>Sobre mim</p>
+            <p>Eu sou um aficcionado por desenvolvimento de software e tecnologia. Sempre em busca de novos desefios </p>
+            <p>Beyond coding, I'm a coffee enthusiast, a cat lover, and a self-taught artist who enjoys
+                spending my free time doodling. I am currently seeking opportunities to bring my skills
+                and enthusiasm to a tech company in the United States or Europe and am
+                excited about the prospect of relocating to pursue new challenges.
+            </p>
+            <button className="bg-[#8A42DB] text-white py-2 px-4 rounded hover:bg-blue-700">
+                Meu currículo
+            </button>
         </div>
     )
 }
