3994f188fdb1d1cc6e893389a7a5a47ca96ef05f
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-21 00:39:58 -0300

    Estilização do titulo

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,7 +1,7 @@
 export function Projects() {
     return (
         <div className="flex flex-col items-center">
-            <h2>Projetos</h2>
+            <h2 className="text-[#7DFFAF] text-base">Projetos</h2>
             <h1>Dê uma olhada nos meus projetos em destaque</h1>
         </div>
     )
