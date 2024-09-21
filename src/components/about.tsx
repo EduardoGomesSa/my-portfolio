c8ec5be077d3d3d9328a323c68ec540af3984479
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-21 00:12:48 -0300

    Imagem provisoria adicionada no Sobre

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,22 +1,32 @@
 export function About() {
     return (
         <div className="w-full h-full flex justify-center items-center">
-            <div className="w-[90vw] bg-white bg-opacity-10 p-8 rounded-lg shadow-lg mx-auto mt-10">
-                <div>
-                    <p>tecnologias</p>
+            {/* Container principal */}
+            <div className="w-[90vw] bg-white bg-opacity-10 p-8 rounded-lg shadow-lg mx-auto mt-10 flex">
+                
+                {/* Parte esquerda - Imagem */}
+                <div className="w-1/2 h-full bg-cover bg-center rounded-lg">
+                    <img src="/assets/nerd.png" alt="Imagem do ilustrativa do sobre" />
+                    {/* A imagem será usada como plano de fundo */}
                 </div>
-                <p>Sobre mim</p>
-                <p>Eu sou um aficcionado por desenvolvimento de software e tecnologia. Sempre em busca de novos desefios </p>
-                <p>Beyond coding, I'm a coffee enthusiast, a cat lover, and a self-taught artist who enjoys
-                    spending my free time doodling. I am currently seeking opportunities to bring my skills
-                    and enthusiasm to a tech company in the United States or Europe and am
-                    excited about the prospect of relocating to pursue new challenges.
-                </p>
-                <button className="bg-[#8A42DB] text-white py-2 px-4 rounded hover:bg-blue-700">
-                    Meu currículo
-                </button>
+
+                {/* Parte direita - Texto */}
+                <div className="w-1/2 flex flex-col justify-center pl-8">
+                    <div>
+                        <p>Tecnologias</p>
+                    </div>
+                    <p>Sobre mim</p>
+                    <p>Eu sou um aficcionado por desenvolvimento de software e tecnologia. Sempre em busca de novos desafios.</p>
+                    <p>Beyond coding, I'm a coffee enthusiast, a cat lover, and a self-taught artist who enjoys
+                        spending my free time doodling. I am currently seeking opportunities to bring my skills
+                        and enthusiasm to a tech company in the United States or Europe and am
+                        excited about the prospect of relocating to pursue new challenges.
+                    </p>
+                    <button className="bg-[#8A42DB] text-white py-2 px-4 rounded hover:bg-blue-700 mt-4">
+                        Meu currículo
+                    </button>
+                </div>
             </div>
         </div>
-
     )
 }
