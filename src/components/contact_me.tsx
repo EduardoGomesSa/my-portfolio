fb71a824260148acf3f8ffd8c814280a8bc6c2e1
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-23 23:34:08 -0300

    Formatação do texto do formulário

--- src/components/contact_me.tsx
+++ src/components/contact_me.tsx
@@ -38,36 +38,36 @@ export function ContactMe() {
                 <div className="w-1/2 flex flex-col justify-center pl-8">
                     <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">Nome</label>
                             <input
+                                placeholder='Nome'
                                 type="text"
                                 name="name"
                                 value={formData.name}
                                 onChange={handleChange}
-                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                className="mt-1 block w-full px-3 py-2 bg-[#171023] text-[#CDD0D4] placeholder-[#CDD0D4] border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 required
                             />
                         </div>
 
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">Email</label>
                             <input
+                                placeholder='E-mail'
                                 type="email"
                                 name="email"
                                 value={formData.email}
                                 onChange={handleChange}
-                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                className="mt-1 block w-full px-3 py-2 bg-[#171023] text-[#CDD0D4] placeholder-[#CDD0D4] border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 required
                             />
                         </div>
 
                         <div>
-                            <label className="block text-sm font-medium text-gray-700">Mensagem</label>
                             <textarea
+                                placeholder='Sua mensagem'
                                 name="message"
                                 value={formData.message}
                                 onChange={handleChange}
-                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                className="mt-1 block w-full px-3 py-2 bg-[#171023] text-[#CDD0D4] placeholder-[#CDD0D4] border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 rows={4}
                                 required
                             />
