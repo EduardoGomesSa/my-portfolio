549586778c6795978fcb7a4b099237c41232a55c
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-12-28 22:24:09 -0300

    Modificando espaçamento dos itens de apresentation

--- src/components/apresentation.tsx
+++ src/components/apresentation.tsx
@@ -3,9 +3,9 @@ export function Apresentation() {
         <div className="w-full h-full flex justify-center items-center">
             <div className="w-[90vw] p-8 rounded-lg shadow-lg mx-auto mt-10 flex">
                 <div className="w-1/2 flex flex-col justify-center pl-8">
-                    <h1 className="text-4xl text-[#4E4563]">Olá! Eu sou Eduardo</h1>
-                    <h2>Desenvolvedor Full-stack e entusasta de tecnologia e inovação</h2>
-                    <p>Já estou na área a mais de 2 anos. Trabalho com desenvilmento de
+                    <h1 className="text-4xl text-[#4E4563] pb-14">Olá! Eu sou Eduardo</h1>
+                    <h2 className="text-2xl text-white pb-6">Desenvolvedor Full-stack e entusasta de tecnologia e inovação</h2>
+                    <p className="pb-6">Já estou na área a mais de 2 anos. Trabalho com desenvilmento de
                         aplicativos e sites, utilizando as seguintes tecnologias Flutter,
                         React, Laravel e MySQL
                     </p>
